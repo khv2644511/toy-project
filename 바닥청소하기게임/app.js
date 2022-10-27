30b50f46b340e78e6b170106f3dc94f9b38eb3c2
khv2644511 <khv2644511@naver.com>
2022-10-27 09:39:41 +0900

    clean x,y

--- 바닥청소하기게임/app.js
+++ 바닥청소하기게임/app.js
@@ -1,6 +1,7 @@
 const target = document.querySelector(".target");
 const body = document.querySelector("body");
-let pos = 0;
+let posx = 0;
+let posy = 0;
 
 // 사용자가 누르는 키의 상태를 저장하는 객체
 let keys = {};
@@ -8,46 +9,76 @@ let keys = {};
 
 function move(event) {
   keys[event.key] = true;
+  // xy[event.rect];
   console.log(keys);
+  console.log(event);
 }
 
 function stop(event) {
   keys[event.key] = false;
   console.log(keys);
 }
-function clean(event) {
+function cleanX(event) {
+  //   console.log(`현재 창기준 x좌표 : ${rect.x}px y좌표 : ${rect.y}px`);
   let rect = target.getBoundingClientRect();
   const clean = document.querySelector(".clean");
-
-  //   console.log(`현재 창기준 x좌표 : ${rect.x}px y좌표 : ${rect.y}px`);
-  //   const x = rect.x;
   console.log(rect);
-  //   clean.style.left = `${rect.x}px`;
-  clean.style.width = `calc(80px + ${rect.x}px)`;
-  //   clean.style.backgroundColor = "red";
+  clean.style.transform = `translate(0px,${posy}px)`;
+
+  clean.style.width = `calc(90px + ${rect.x}px)`;
+  clean.style.height = `calc(80px)`;
 }
+function cleanY(event) {
+  //   console.log(`현재 창기준 x좌표 : ${rect.x}px y좌표 : ${rect.y}px`);
+  let rect = target.getBoundingClientRect();
+  const clean = document.querySelector(".clean");
+  const height = window.innerHeight;
+  console.log(rect);
+  clean.style.transform = `translate(${posx}px,0px)`;
+  clean.style.width = `calc(90px)`;
+  clean.style.height = `calc(90px + ${rect.y}px)`;
+}
+
 document.addEventListener("keydown", move);
 document.addEventListener("keyup", stop);
-document.addEventListener("keydown", clean);
+document.addEventListener("keydown", cleanX);
+document.addEventListener("keydown", cleanY);
 
 function play() {
-  clean();
-
   if (keys.ArrowRight) {
-    pos += 5;
-    target.style.transform = `translateX(${pos}px)`;
+    posx += 5;
+    target.style.transform = `translate(${posx}px,${posy}px)`;
+    cleanX();
   }
   if (keys.ArrowLeft) {
-    pos -= 5;
-    target.style.transform = `translateX(${pos}px)`;
+    posx -= 5;
+    target.style.transform = `translate(${posx}px,${posy}px)`;
+    cleanX();
   }
+  if (posx <= 0) {
+    posx = 0;
+  }
+
+  if (posx >= window.innerWidth - 100) {
+    posx = window.innerWidth - 100;
+  }
+
   if (keys.ArrowUp) {
-    pos -= 5;
-    target.style.transform = `translateY(${pos}px)`;
+    posy -= 5;
+    target.style.transform = `translate(${posx}px,${posy}px)`;
+    cleanY();
   }
   if (keys.ArrowDown) {
-    pos += 5;
-    target.style.transform = `translateY(${pos}px)`;
+    posy += 5;
+    target.style.transform = `translate(${posx}px,${posy}px)`;
+    cleanY();
+  }
+  if (posy <= 0) {
+    posy = 0;
+  }
+
+  if (posy >= window.innerHeight - 100) {
+    posy = window.innerHeight - 100;
   }
   console.log("play!");
   window.requestAnimationFrame(play);
